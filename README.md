2699a9a58175654edaff375a1081ba59e41c0911
Glitch (hello-express) <none>
2021-04-13 17:01:02 +0000

    🚥🍀 Checkpoint ./README.md:1942716/1420 ./views/index.html:1942716/2038 ./public/script.js:1942716/1865

--- README.md
+++ README.md
@@ -1,30 +1,5 @@
-# hello-express
+# Kahot
 
-A server that serves a webpage, its resources, and some data
+An alternate kahoot client using socket.io and kahoot.js-updated
 
-
-## Your Project
-
-On the front-end,
-
-- Edit `views/index.html` to change the content of the webpage
-- `public/client.js` is the javacript that runs when you load the webpage
-- `public/style.css` is the styles for `views/index.html`
-- Drag in `assets`, like images or music, to add them to your project
-
-On the back-end,
-
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
-
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy.
-
-
-## Made by [Glitch](https://glitch.com/)
-
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
-
-Find out more [about Glitch](https://glitch.com/about).
-
-( ᵔ ᴥ ᵔ )
+May not work in the future (writing this April 2021) as the kahoot api package I'm using is no longer maintained. If I find another kahoot api package, I will definitely do a rewrite with the new one. 
--- public/script.js
+++ public/script.js
@@ -1,41 +1,20 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
+// The client-side javascript
+/*
+This code will probably be very messy, as i'm not too familiar 
+with socket.io and things like it.
 
-console.log("hello world :o");
+*/
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
+var socket = io();
+// If your IDE says that io is not defined, dont worry about it, we import it in the html.
 
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
+class Game {
+  constructor(){
+    this.socket = socket;
+    this.state = 1;
+    /*
+    Game states:
+    1: Waiting for pin to be entered
+    */
+  }
 }
-
-// fetch the initial list of dreams
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
-
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
-  });
