79b6cac7e54e90e46c124727a1cf649c2907c1ac
Glitch (hello-express) <none>
2021-04-14 20:23:18 +0000

    👖☕️ Checkpoint ./public/script.js:1942716/20

--- public/script.js
+++ public/script.js
@@ -75,7 +75,7 @@ class Game {
       case 3: $("#loading").show(); $("#loadingMessage")[0].innerHTML = "You're In!"; break;
       case 4: $("#loading").show(); $("#loadingMessage")[0].innerHTML = "Question starting..."; break;
       case 5: $("#question").show(); break;
-      case 6: $("#loading").show(); $("#loadingMessage")[0].innerHTML = answertext[Math.floor(Math.random()*3)-1]; break;
+      case 6: $("#loading").show(); $("#loadingMessage")[0].innerHTML = answertext[Math.floor(Math.random()*answertext.length)]; break;
       case 8: $("#loading").show(); $("#loadingMessage")[0].innerHTML = "Joining game..."; break;
       case 9: $("#loading").show(); $("#loadingMessage")[0].innerHTML = "Quiz started!"; break;
     }
